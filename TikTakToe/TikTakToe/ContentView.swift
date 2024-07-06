c68614ba2be3e91cdb58c4cb4604a0e16bc34d5c
Ishaan Dhiman <151793655+IshaanDCode@users.noreply.github.com>
2024-07-06 14:51:35 -0400

    Add files via upload

---
+++ TikTakToe/TikTakToe/ContentView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        var borderSize = CGFloat(5)
+        
+        VStack (spacing: borderSize)
+        {
+            
+            ForEach(0...2, id: \.self)
+            {
+                row in
+                HStack(spacing: borderSize){
+                    ForEach(0...2, id: \.self)
+                    {
+                        column in
+                        Text("X")
+                            .font(.system(size: 60))
+                            .bold()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+
+                    }
+
+                }
+            }
+        }
+        .background(Color.black)
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+    
---
+++ TikTakToe/TikTakToe/TikTakToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTakToeApp.swift
+//  TikTakToe
+//
+//  Created by Ishaan on 2024-07-06.
+//
+
+import SwiftUI
+
+@main
+struct TikTakToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
