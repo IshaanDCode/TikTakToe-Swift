e714f936f2f3d858a42b28f8b68502ee2397655c
Ishaan Dhiman <ishaandhiman74@gmail.com>
2024-07-08 09:46:19 -0400

    Added comments

--- TikTakToe/TikTakToe/Cell.swift
+++ TikTakToe/TikTakToe/Cell.swift
@@ -1,39 +1,37 @@
 import Foundation
 import SwiftUI
 
-struct Cell
-{
-    var tile: Tile
+// Define a struct `Cell` representing a single cell in the Tic-Tac-Toe game board
+struct Cell {
+    var tile: Tile // Represents the current state of the cell: Nought, Cross, or Empty
     
-    func displayTile() -> String
-    {
-        switch(tile)
-        {
-            case Tile.Nought:
-                    return "O"
-            case Tile.Cross:
-                return "X"
-            default:
-                return ""
+    // Function to display the content of the cell based on its `tile` value
+    func displayTile() -> String {
+        switch tile {
+        case .Nought:
+            return "O" // Display "O" if the cell contains a Nought
+        case .Cross:
+            return "X" // Display "X" if the cell contains a Cross
+        case .Empty:
+            return "" // Display an empty string if the cell is empty
         }
     }
     
-    func tileColor() -> Color
-    {
-        switch(tile)
-        {
-            case Tile.Nought:
-                return Color.red
-            case Tile.Cross:
-                return Color.black
-            default:
-                return Color.black
+    // Function to determine the color of the tile based on its `tile` value
+    func tileColor() -> Color {
+        switch tile {
+        case .Nought:
+            return Color.red // Set color to red for Noughts
+        case .Cross:
+            return Color.black // Set color to black for Crosses
+        case .Empty:
+            return Color.black // Default color (black) for empty cells
         }
     }
 }
 
-enum Tile
-{
+// Enum `Tile` represents the possible states of a cell: Nought, Cross, or Empty
+enum Tile {
     case Nought
     case Cross
     case Empty
